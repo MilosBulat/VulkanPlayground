68db17d0a0d238ebed728751ea808d337ac9573a
Milos Bulat <milos.bulat@foundry.com>
2024-10-19 16:00:43 +0200

    Shaders, compilation and loading

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <optional>
 #include <algorithm> // Necessary for std::clamp
+#include <fstream>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -63,6 +64,30 @@ private:
         selectPhyiscalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
+        createGraphicsPipeline();
+    }
+
+    static std::vector<char> readFile(const std::string& filename) {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open()) {
+            throw std::runtime_error("failed to open file!");
+        }
+
+        size_t fileSize = (size_t)file.tellg();
+        std::vector<char> buffer(fileSize);
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+
+        file.close();
+
+        return buffer;
+    }
+
+    void createGraphicsPipeline() {
+        auto vertShaderCode = readFile("shaders/shaderVert.spv");
+        auto fragShaderCode = readFile("shaders/shaderFrag.spv");
     }
 
     void createInstance() {
